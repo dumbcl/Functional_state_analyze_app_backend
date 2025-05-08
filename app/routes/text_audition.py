5abbf0ef18e6892f5801aa18f3875db12763a8e5
Dumbcl <kostfok@mail.ru>
2025-05-08 19:39:31 +0300

    another log

--- app/routes/text_audition.py
+++ app/routes/text_audition.py
@@ -99,6 +99,10 @@ async def post_text_audition_result(
         with open('logs.txt', 'a') as  file:
             file.write("transcripted\n")
         comparer = TextComparer(language='russian')
+        with open('logs.txt', 'a') as  file:
+            file.write("transcripted o read_list[read_text_index]\n")
+        with open('logs.txt', 'a') as  file:
+            file.write("transcripted a read_list[read_text_index]\n")
         read_analysis = comparer.analyze(read_list[read_text_index], transcript_read)
         repeat_analysis = comparer.analyze(repeat_list[repeat_text_index], transcript_repeat)
         quality_score_read = read_analysis['scores']['overall_score']/100
