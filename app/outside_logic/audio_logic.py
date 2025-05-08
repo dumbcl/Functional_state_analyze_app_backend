1aaab8cc1fa7daf7622316a0f5d746f0093a772e
Dumbcl <kostfok@mail.ru>
2025-05-08 19:45:19 +0300

    credentials

--- app/outside_logic/audio_logic.py
+++ app/outside_logic/audio_logic.py
@@ -17,14 +17,10 @@ from speechkit import model_repository, configure_credentials, creds
 from speechkit.stt import AudioProcessingType
 import os
 
-# Authentication with an API key.
-configure_credentials(
-   yandex_credentials=creds.YandexCredentials(
-      api_key=os.getenv("YANDEX_API_KEY")
-   )
-)
-
 def recognize(audio):
+   configure_credentials(
+       yandex_credentials=creds.YandexCredentials(api_key=os.getenv("YANDEX_API_KEY"))
+   )
    model = model_repository.recognition_model()
 
    # Specify the recognition settings.
