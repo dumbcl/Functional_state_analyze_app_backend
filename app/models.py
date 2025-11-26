f52a35e78ef44574be4f2877b92b06ab95331fba
Dumbcl <kostfok@mail.ru>
2025-11-26 21:59:09 +0300

    fix models

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from typing import List, Tuple
 from sqlalchemy import Column, Integer, String, ForeignKey, Float, DateTime, Date, Boolean, Text, UniqueConstraint
 from sqlalchemy.orm import relationship
 from datetime import datetime, date
-from database import Base
+from app.gdatabase import Base
 
 class User(Base):
     __tablename__ = "users"
